aa1c1029a95c1e0605282a673bdc8198036682b8
Uxlabspk <muhammadnaveedcis@gmail.com>
2024-05-01 13:59:52 +0500

    Adding first component

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
         <img src="Camera.png" alt="null" width={120} />
         <div className="mt-3 text-center">
           <h1 className="font-medium text-[22px]">Allow Notifications</h1>
-          <p className="leading-5 mt-2 line-hieght text-center text-gray-800">The majestic Rocky Mountains are a major tourist location in the western United States.</p>
+          <p className="leading-5 mt-2 text-center text-gray-800">The majestic Rocky Mountains are a major tourist location in the western United States.</p>
         </div>
         <button className="mt-5 bg-indigo-500 text-white text-md font-medium py-3 px-28 rounded-lg uppercase">Allow</button>
         <button className="mt-2 text-indigo-500 text-md font-medium py-3 px-23 rounded-lg uppercase">Not Now</button>
