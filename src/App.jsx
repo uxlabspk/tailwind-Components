19043c7cc0efb5eb148445b1bcaa482d40e08524
Uxlabspk <muhammadnaveedcis@gmail.com>
2024-05-01 13:49:24 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+
+function App() {
+  return (
+    <>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="p-10 w-80 flex flex-col items-center justify-center py-22 shadow rounded-md">
+        <img src="Camera.png" alt="null" width={120} />
+        <div className="mt-3 text-center">
+          <h1 className="font-medium text-[22px]">Allow Notifications</h1>
+          <p className="leading-5 mt-2 line-hieght text-center text-gray-800">The majestic Rocky Mountains are a major tourist location in the western United States.</p>
+        </div>
+        <button className="mt-5 bg-indigo-500 text-white text-md font-medium py-3 px-28 rounded-lg uppercase">Allow</button>
+        <button className="mt-2 text-indigo-500 text-md font-medium py-3 px-23 rounded-lg uppercase">Not Now</button>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
