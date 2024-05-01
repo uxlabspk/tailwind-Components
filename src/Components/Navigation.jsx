465667d78a2b5a68de83fe70ccf8fa45b4489acc
Uxlabspk <muhammadnaveedcis@gmail.com>
2024-05-01 14:47:42 +0500

    Adding second component

---
+++ src/Components/Navigation.jsx
@@ -0,0 +1,16 @@
+
+
+function Navigation() {
+    return (
+        <div className="bg-white shadow-sm mt-5 px-4 py-3 w-96">
+            <div className="flex justify-between items-center">
+                <img src="/Back.png" alt="goBack" />
+                <span className="text-[17px] font-semibold">Navigation</span>
+                <img src="/Navigation.png" alt="navigation" />
+            </div>
+        </div>
+    )
+}
+
+
+export default Navigation;
